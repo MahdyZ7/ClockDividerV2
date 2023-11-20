1e93a220f0c70caeba4392cc6745bc0b1a692b9b
Ahmed Hassan <amahdy9@gmail.com>
2023-11-20 18:29:52 +0400

    synthasis done

--- BigDiv/netlists/BigDiv.v
+++ BigDiv/netlists/BigDiv.v
@@ -1,48 +1,62 @@
 /////////////////////////////////////////////////////////////
-// Created by: Synopsys Design Compiler(R)
+// Created by: Synopsys DC Ultra(TM) in wire load mode
 // Version   : S-2021.06-SP5-5
-// Date      : Mon Nov 20 16:03:46 2023
+// Date      : Mon Nov 20 17:56:39 2023
 /////////////////////////////////////////////////////////////
+
+
+module CLKDivider_Div9alt_1 ( reset, clk, div9, VDD, VSS );
+  input reset, clk;
+  output div9;
+  inout VDD,  VSS;
+  wire   midC, midD, midA, midB, t1, N11, N13, t2, n10, n12, n14, n16, n34,
+         n35, n36, n37, n38, n39, n40, n41, n42;
+
+  SC8T_DFFNQX1_CSC28L t2_reg ( .D(n10), .CLK(clk), .Q(t2) );
+  SC8T_DFFQX1_CSC28L midA_reg ( .D(n16), .CLK(clk), .Q(midA) );
+  SC8T_DFFQX1_CSC28L midC_reg ( .D(n14), .CLK(clk), .Q(midC) );
+  SC8T_DFFQX1_CSC28L midD_reg ( .D(N13), .CLK(clk), .Q(midD) );
+  SC8T_DFFQX1_CSC28L midB_reg ( .D(N11), .CLK(clk), .Q(midB) );
+  SC8T_DFFQX1_CSC28L t1_reg ( .D(n12), .CLK(clk), .Q(t1) );
+  SC8T_INVX1_MR_CSC28L U3 ( .A(midD), .Z(n42) );
+  SC8T_INVX1_MR_CSC28L U4 ( .A(midC), .Z(n36) );
+  SC8T_NR2X1_CSC24L U5 ( .A(n42), .B(n36), .Z(n35) );
+  SC8T_OAI21X1_MR_CSC24L U6 ( .B1(t2), .B2(n35), .A(reset), .Z(n34) );
+  SC8T_AOI21X1_MR_CSC24L U7 ( .B1(t2), .B2(n35), .A(n34), .Z(n10) );
+  SC8T_ND2X1_CSC28L U8 ( .A(reset), .B(midA), .Z(n40) );
+  SC8T_AOI21X1_MR_CSC24L U9 ( .B1(midB), .B2(n36), .A(n40), .Z(n14) );
+  SC8T_NR2X1_CSC24L U10 ( .A(midD), .B(midC), .Z(n38) );
+  SC8T_OAI21X1_MR_CSC24L U11 ( .B1(t1), .B2(n38), .A(reset), .Z(n37) );
+  SC8T_AOI21X1_MR_CSC24L U12 ( .B1(t1), .B2(n38), .A(n37), .Z(n12) );
+  SC8T_INVX1_MR_CSC28L U13 ( .A(reset), .Z(n41) );
+  SC8T_NR2X1_CSC24L U14 ( .A(midC), .B(n41), .Z(N13) );
+  SC8T_OA22IA1A2X1_MR_CSC28L U15 ( .A1(t1), .A2(t2), .B1(t2), .B2(t1), .Z(div9) );
+  SC8T_INVX1_MR_CSC28L U16 ( .A(midB), .Z(n39) );
+  SC8T_OAI32X1_MR_CSC24L U17 ( .B1(n39), .B2(n42), .B3(n41), .A1(midB), .A2(
+        n40), .Z(n16) );
+  SC8T_OAI32X1_MR_CSC24L U18 ( .B1(n42), .B2(midA), .B3(n41), .A1(midD), .A2(
+        n40), .Z(N11) );
+endmodule
 
 
 module CLKDivider_Div12_1 ( reset, clk, div12, VDD, VSS );
   input reset, clk;
   output div12;
   inout VDD,  VSS;
-  wire   N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11;
+  wire   N6, N7, N8, n4, n10, n11, n12;
   wire   [2:0] count;
-  tri   reset;
-  tri   clk;
 
-  \**SEQGEN**  div12_reg ( .clear(1'b0), .preset(1'b0), .next_state(N10), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div12), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(N9) );
-  \**SEQGEN**  count_reg_2_ ( .clear(1'b0), .preset(1'b0), .next_state(N8), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count[2]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count_reg_1_ ( .clear(1'b0), .preset(1'b0), .next_state(N7), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count[1]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count_reg_0_ ( .clear(1'b0), .preset(1'b0), .next_state(N6), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count[0]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  SELECT_OP C25 ( .DATA1({1'b0, 1'b0, 1'b0}), .DATA2({count[0], N2, count[1]}), 
-        .CONTROL1(N0), .CONTROL2(N1), .Z({N8, N7, N6}) );
-  GTECH_BUF B_0 ( .A(N3), .Z(N0) );
-  GTECH_BUF B_1 ( .A(reset), .Z(N1) );
-  SELECT_OP C26 ( .DATA1(1'b1), .DATA2(N4), .CONTROL1(N0), .CONTROL2(N1), .Z(
-        N9) );
-  SELECT_OP C27 ( .DATA1(1'b0), .DATA2(N5), .CONTROL1(N0), .CONTROL2(N1), .Z(
-        N10) );
-  GTECH_NOT I_0 ( .A(count[2]), .Z(N2) );
-  GTECH_NOT I_1 ( .A(reset), .Z(N3) );
-  GTECH_AND2 C34 ( .A(N2), .B(N11), .Z(N4) );
-  GTECH_NOT I_2 ( .A(count[1]), .Z(N11) );
-  GTECH_NOT I_3 ( .A(div12), .Z(N5) );
+  SC8T_DFFQX1_CSC28L count_reg_0_ ( .D(N6), .CLK(clk), .Q(count[0]) );
+  SC8T_DFFQX1_CSC28L count_reg_2_ ( .D(N8), .CLK(clk), .Q(count[2]) );
+  SC8T_DFFQX1_CSC28L count_reg_1_ ( .D(N7), .CLK(clk), .Q(count[1]) );
+  SC8T_DFFQX1_CSC28L div12_reg ( .D(n4), .CLK(clk), .Q(div12) );
+  SC8T_INVX1_MR_CSC28L U3 ( .A(reset), .Z(n10) );
+  SC8T_NR2IAX1_MR_CSC28L U4 ( .A(count[1]), .B(n10), .Z(N6) );
+  SC8T_NR2IAX1_MR_CSC28L U5 ( .A(count[0]), .B(n10), .Z(N8) );
+  SC8T_NR2X1_CSC24L U6 ( .A(count[2]), .B(n10), .Z(N7) );
+  SC8T_NR2X1_CSC24L U7 ( .A(count[2]), .B(count[1]), .Z(n12) );
+  SC8T_OAI21X1_MR_CSC24L U8 ( .B1(div12), .B2(n12), .A(reset), .Z(n11) );
+  SC8T_AOI21X1_MR_CSC24L U9 ( .B1(div12), .B2(n12), .A(n11), .Z(n4) );
 endmodule
 
 
@@ -50,80 +64,39 @@ module CLKDivider_Div80alt_1 ( reset, clk, div80, div4, div8, VDD, VSS );
   input reset, clk;
   output div80, div4, div8;
   inout VDD,  VSS;
-  wire   N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, N13, N14, N15,
-         N16, N17, N18, N19, N20, N21, N22, N23, N24, N25, N26, N27, N28, N29;
+  wire   count8_2_, count8_1_, N13, N16, N17, N18, N21, n9, n11, n13, n27, n28,
+         n29, n30, n31, n32, n33, n34;
   wire   [2:0] count5;
-  wire   [2:0] count8;
-  tri   reset;
-  tri   clk;
 
-  \**SEQGEN**  div4_reg ( .clear(1'b0), .preset(1'b0), .next_state(N13), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div4), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  div80_reg ( .clear(1'b0), .preset(1'b0), .next_state(N20), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div80), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(N19) );
-  \**SEQGEN**  count5_reg_2_ ( .clear(1'b0), .preset(1'b0), .next_state(N18), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count5[2]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count5_reg_1_ ( .clear(1'b0), .preset(1'b0), .next_state(N17), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count5[1]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count5_reg_0_ ( .clear(1'b0), .preset(1'b0), .next_state(N16), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count5[0]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  div8_reg ( .clear(1'b0), .preset(1'b0), .next_state(N21), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div8), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count8_reg_2_ ( .clear(1'b0), .preset(1'b0), .next_state(N15), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count8[2]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count8_reg_1_ ( .clear(1'b0), .preset(1'b0), .next_state(N14), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count8[1]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  \**SEQGEN**  count8_reg_0_ ( .clear(1'b0), .preset(1'b0), .next_state(N13), 
-        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(count8[0]), 
-        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
-        .synch_enable(1'b1) );
-  GTECH_NOT I_0 ( .A(count5[2]), .Z(N22) );
-  GTECH_OR2 C39 ( .A(count8[1]), .B(count8[2]), .Z(N23) );
-  GTECH_OR2 C40 ( .A(count8[0]), .B(N23), .Z(N24) );
-  GTECH_NOT I_1 ( .A(N24), .Z(N25) );
-  SELECT_OP C42 ( .DATA1({1'b0, 1'b0, 1'b0}), .DATA2({N9, N10, N5}), 
-        .CONTROL1(N0), .CONTROL2(N1), .Z({N15, N14, N13}) );
-  GTECH_BUF B_0 ( .A(N6), .Z(N0) );
-  GTECH_BUF B_1 ( .A(reset), .Z(N1) );
-  SELECT_OP C43 ( .DATA1({1'b0, 1'b0, 1'b0}), .DATA2({N7, count5[0], N8}), 
-        .CONTROL1(N0), .CONTROL2(N1), .Z({N18, N17, N16}) );
-  SELECT_OP C44 ( .DATA1(1'b1), .DATA2(N11), .CONTROL1(N0), .CONTROL2(N1), .Z(
-        N19) );
-  SELECT_OP C45 ( .DATA1(1'b0), .DATA2(N12), .CONTROL1(N0), .CONTROL2(N1), .Z(
-        N20) );
-  SELECT_OP C46 ( .DATA1(1'b0), .DATA2(N4), .CONTROL1(N0), .CONTROL2(N1), .Z(
-        N21) );
-  GTECH_NOT I_2 ( .A(count5[1]), .Z(N2) );
-  GTECH_AND2 C50 ( .A(count8[1]), .B(N26), .Z(N3) );
-  GTECH_NOT I_3 ( .A(count8[0]), .Z(N26) );
-  GTECH_NOT I_4 ( .A(count8[2]), .Z(N4) );
-  GTECH_NOT I_5 ( .A(N27), .Z(N5) );
-  GTECH_XOR2 C54 ( .A(count8[1]), .B(count8[2]), .Z(N27) );
-  GTECH_NOT I_6 ( .A(reset), .Z(N6) );
-  GTECH_OR2 C58 ( .A(N2), .B(count5[0]), .Z(N7) );
-  GTECH_AND2 C59 ( .A(count5[2]), .B(N2), .Z(N8) );
-  GTECH_OR2 C60 ( .A(N3), .B(N28), .Z(N9) );
-  GTECH_AND2 C61 ( .A(count8[2]), .B(count8[0]), .Z(N28) );
-  GTECH_OR2 C62 ( .A(N3), .B(N29), .Z(N10) );
-  GTECH_AND2 C63 ( .A(N4), .B(count8[0]), .Z(N29) );
-  GTECH_AND2 C64 ( .A(N22), .B(N25), .Z(N11) );
-  GTECH_NOT I_7 ( .A(div80), .Z(N12) );
+  SC8T_DFFQX1_CSC28L div80_reg ( .D(n9), .CLK(clk), .Q(div80) );
+  SC8T_DFFQX1_CSC28L div8_reg ( .D(N21), .CLK(clk), .Q(div8) );
+  SC8T_DFFQX1_CSC28L count5_reg_0_ ( .D(N16), .CLK(clk), .Q(count5[0]) );
+  SC8T_DFFQX1_CSC28L count5_reg_2_ ( .D(N18), .CLK(clk), .Q(count5[2]) );
+  SC8T_DFFQX1_CSC28L count5_reg_1_ ( .D(N17), .CLK(clk), .Q(count5[1]) );
+  SC8T_DFFQX1_CSC28L count8_reg_2_ ( .D(n11), .CLK(clk), .Q(count8_2_) );
+  SC8T_DFFQX1_CSC28L count8_reg_1_ ( .D(n13), .CLK(clk), .Q(count8_1_) );
+  SC8T_DFFQX1_CSC28L count8_reg_0_ ( .D(N13), .CLK(clk), .Q(div4) );
+  SC8T_INVX1_MR_CSC28L U3 ( .A(reset), .Z(n32) );
+  SC8T_NR3IAX1_MR_CSC28L U4 ( .A(count5[2]), .B(count5[1]), .C(n32), .Z(N16)
+         );
+  SC8T_ND2X1_CSC28L U5 ( .A(reset), .B(count5[0]), .Z(n27) );
+  SC8T_OAI21X1_MR_CSC24L U6 ( .B1(count5[1]), .B2(n32), .A(n27), .Z(N18) );
+  SC8T_NR2X1_CSC24L U7 ( .A(count8_2_), .B(n32), .Z(N21) );
+  SC8T_INVX1_MR_CSC28L U8 ( .A(n27), .Z(N17) );
+  SC8T_NR4X1_CSC24L U9 ( .A(count8_2_), .B(count8_1_), .C(count5[2]), .D(div4), 
+        .Z(n29) );
+  SC8T_OAI21X1_MR_CSC24L U10 ( .B1(div80), .B2(n29), .A(reset), .Z(n28) );
+  SC8T_AOI21X1_MR_CSC24L U11 ( .B1(div80), .B2(n29), .A(n28), .Z(n9) );
+  SC8T_INVX1_MR_CSC28L U12 ( .A(N21), .Z(n30) );
+  SC8T_INVX1_MR_CSC28L U13 ( .A(count8_2_), .Z(n33) );
+  SC8T_ND2X1_CSC28L U14 ( .A(reset), .B(count8_1_), .Z(n31) );
+  SC8T_OAI22X1_CSC24L U15 ( .A1(count8_1_), .A2(n30), .B1(n33), .B2(n31), .Z(
+        N13) );
+  SC8T_INVX1_MR_CSC28L U16 ( .A(div4), .Z(n34) );
+  SC8T_AOI22X1_CSC24L U17 ( .A1(div4), .A2(n30), .B1(n31), .B2(n34), .Z(n13)
+         );
+  SC8T_OAI32X1_MR_CSC24L U18 ( .B1(n34), .B2(n33), .B3(n32), .A1(div4), .A2(
+        n31), .Z(n11) );
 endmodule
 
 
@@ -132,13 +105,11 @@ module dividerblock ( reset, clk, VDD, VSS, div4, div8, div9, div12, div80 );
   output div4, div8, div9, div12, div80;
   inout VDD,  VSS;
 
-  tri   reset;
-  tri   clk;
   tri   VDD;
   tri   VSS;
-  tri   div9;
 
-  Div9alt d9 ( .reset(reset), .clk(clk), .div9(div9), .VDD(VDD), .VSS(VSS) );
+  CLKDivider_Div9alt_1 d9 ( .reset(reset), .clk(clk), .div9(div9), .VDD(VDD), 
+        .VSS(VSS) );
   CLKDivider_Div12_1 d12 ( .reset(reset), .clk(clk), .div12(div12), .VDD(VDD), 
         .VSS(VSS) );
   CLKDivider_Div80alt_1 d80 ( .reset(reset), .clk(clk), .div80(div80), .div4(
--- BigDiv/unmapped/dividerblock_chip_unmapped.v
+++ BigDiv/unmapped/dividerblock_chip_unmapped.v
@@ -1,8 +1,78 @@
 /////////////////////////////////////////////////////////////
 // Created by: Synopsys Design Compiler(R)
 // Version   : S-2021.06-SP5-5
-// Date      : Mon Nov 20 16:34:43 2023
+// Date      : Mon Nov 20 17:18:02 2023
 /////////////////////////////////////////////////////////////
+
+
+module Div9alt ( reset, clk, div9, VDD, VSS );
+  input reset, clk;
+  output div9;
+  inout VDD,  VSS;
+  wire   N0, N1, midC, midD, N2, midA, midB, N3, N4, t1, N5, N6, N7, N8, N9,
+         N10, N11, N12, N13, N14, N15, N16, N17, N18, N19, t2, N20, N21, N22,
+         N23, N24, N25, N26, N27;
+
+  \**SEQGEN**  midC_reg ( .clear(1'b0), .preset(1'b0), .next_state(N12), 
+        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(midC), 
+        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
+        .synch_enable(1'b1) );
+  \**SEQGEN**  midD_reg ( .clear(1'b0), .preset(1'b0), .next_state(N13), 
+        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(midD), 
+        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
+        .synch_enable(1'b1) );
+  \**SEQGEN**  t1_reg ( .clear(1'b0), .preset(1'b0), .next_state(N15), 
+        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(t1), .synch_clear(
+        1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), .synch_enable(N14) );
+  \**SEQGEN**  midA_reg ( .clear(1'b0), .preset(1'b0), .next_state(N10), 
+        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(midA), 
+        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
+        .synch_enable(1'b1) );
+  \**SEQGEN**  midB_reg ( .clear(1'b0), .preset(1'b0), .next_state(N11), 
+        .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(midB), 
+        .synch_clear(1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), 
+        .synch_enable(1'b1) );
+  \**SEQGEN**  t2_reg ( .clear(1'b0), .preset(1'b0), .next_state(N22), 
+        .clocked_on(N16), .data_in(1'b0), .enable(1'b0), .Q(t2), .synch_clear(
+        1'b0), .synch_preset(1'b0), .synch_toggle(1'b0), .synch_enable(N21) );
+  SELECT_OP C51 ( .DATA1(1'b0), .DATA2(N7), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N10) );
+  GTECH_BUF B_0 ( .A(N4), .Z(N0) );
+  GTECH_BUF B_1 ( .A(reset), .Z(N1) );
+  SELECT_OP C52 ( .DATA1(1'b0), .DATA2(N6), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N11) );
+  SELECT_OP C53 ( .DATA1(1'b0), .DATA2(N5), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N12) );
+  SELECT_OP C54 ( .DATA1(1'b0), .DATA2(N2), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N13) );
+  SELECT_OP C55 ( .DATA1(1'b1), .DATA2(N8), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N14) );
+  SELECT_OP C56 ( .DATA1(1'b0), .DATA2(N9), .CONTROL1(N0), .CONTROL2(N1), .Z(
+        N15) );
+  SELECT_OP C57 ( .DATA1(1'b1), .DATA2(1'b1), .DATA3(1'b0), .CONTROL1(N0), 
+        .CONTROL2(N23), .CONTROL3(N19), .Z(N21) );
+  SELECT_OP C58 ( .DATA1(1'b0), .DATA2(N20), .CONTROL1(N0), .CONTROL2(N23), 
+        .Z(N22) );
+  GTECH_NOT I_0 ( .A(midC), .Z(N2) );
+  GTECH_NOT I_1 ( .A(midB), .Z(N3) );
+  GTECH_NOT I_2 ( .A(reset), .Z(N4) );
+  GTECH_AND2 C66 ( .A(midA), .B(N24), .Z(N5) );
+  GTECH_OR2 C67 ( .A(N3), .B(midC), .Z(N24) );
+  GTECH_XOR2 C68 ( .A(midA), .B(midD), .Z(N6) );
+  GTECH_OR2 C69 ( .A(N25), .B(N26), .Z(N7) );
+  GTECH_AND2 C70 ( .A(midA), .B(N3), .Z(N25) );
+  GTECH_AND2 C71 ( .A(midB), .B(midD), .Z(N26) );
+  GTECH_AND2 C72 ( .A(N2), .B(N27), .Z(N8) );
+  GTECH_NOT I_3 ( .A(midD), .Z(N27) );
+  GTECH_NOT I_4 ( .A(t1), .Z(N9) );
+  GTECH_NOT I_5 ( .A(clk), .Z(N16) );
+  GTECH_AND2 C77 ( .A(midC), .B(midD), .Z(N17) );
+  GTECH_OR2 C79 ( .A(N17), .B(N4), .Z(N18) );
+  GTECH_NOT I_6 ( .A(N18), .Z(N19) );
+  GTECH_NOT I_7 ( .A(t2), .Z(N20) );
+  GTECH_AND2 C83 ( .A(N17), .B(reset), .Z(N23) );
+  GTECH_XOR2 C84 ( .A(t1), .B(t2), .Z(div9) );
+endmodule
 
 
 module Div12 ( reset, clk, div12, VDD, VSS );
@@ -11,8 +81,6 @@ module Div12 ( reset, clk, div12, VDD, VSS );
   inout VDD,  VSS;
   wire   N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11;
   wire   [2:0] count;
-  tri   reset;
-  tri   clk;
 
   \**SEQGEN**  div12_reg ( .clear(1'b0), .preset(1'b0), .next_state(N10), 
         .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div12), 
@@ -54,8 +122,6 @@ module Div80alt ( reset, clk, div80, div4, div8, VDD, VSS );
          N16, N17, N18, N19, N20, N21, N22, N23, N24, N25, N26, N27, N28, N29;
   wire   [2:0] count5;
   wire   [2:0] count8;
-  tri   reset;
-  tri   clk;
 
   \**SEQGEN**  div4_reg ( .clear(1'b0), .preset(1'b0), .next_state(N13), 
         .clocked_on(clk), .data_in(1'b0), .enable(1'b0), .Q(div4), 
@@ -132,11 +198,8 @@ module dividerblock ( reset, clk, VDD, VSS, div4, div8, div9, div12, div80 );
   output div4, div8, div9, div12, div80;
   inout VDD,  VSS;
 
-  tri   reset;
-  tri   clk;
   tri   VDD;
   tri   VSS;
-  tri   div9;
 
   Div9alt d9 ( .reset(reset), .clk(clk), .div9(div9), .VDD(VDD), .VSS(VSS) );
   Div12 d12 ( .reset(reset), .clk(clk), .div12(div12), .VDD(VDD), .VSS(VSS) );
